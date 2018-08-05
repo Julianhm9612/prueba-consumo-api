edbb9130df3445b58db257ab388a121917b8abbf
karhy259 <karina.ospinabonilla@gmail.com>
2018-08-05 15:40:27 -0500

    Agregar componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import { AppRoutingModule } from './app.routing';
+
+// Angular Material
+import {
+  MatToolbarModule,  
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
+
+// componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
